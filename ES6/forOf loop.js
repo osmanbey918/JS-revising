be237094d255aec0ca21835eb0167722875c600a
Usman Khan <usman918khan@gmail.com>
2024-08-07 20:31:49 +0500

    grid array problem

--- ES6/forOf loop.js
+++ ES6/forOf loop.js
@@ -1,26 +1,26 @@
-// for/of loop is mainly used for iterating without index 
-// The JavaScript for of statement loops through the values of an iterable object.
+// // for/of loop is mainly used for iterating without index 
+// // The JavaScript for of statement loops through the values of an iterable object.
 
-// It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists,
+// // It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists,
 
-const cars = ["BMW", "Volvo", "Mini"];
+// const cars = ["BMW", "Volvo", "Mini"];
 
-let text = "";
-for (let x of cars) {
-  text += x;
-}
-console.log(text);
+// let text = "";
+// for (let x of cars) {
+//   text += x;
+// }
+// console.log(text);
 
-var satisfiesConditions = function (grid) {
-  let i = 0;
-  let g = grid
-  for (let j = 1; j < g[0].length; j++) {
-      if (
-          g[i][j - 1] == g[i][j]
-      ) return false
-  }
+// var satisfiesConditions = function (grid) {
+//   let i = 0;
+//   let g = grid
+//   for (let j = 1; j < g[0].length; j++) {
+//       if (
+//           g[i][j - 1] == g[i][j]
+//       ) return false
+//   }
 
-  let s = new Set(g.map(e => e.join('')))
-  if (s.size > 1) return false
-  return true
-};
+//   let s = new Set(g.map(e => e.join('')))
+//   if (s.size > 1) return false
+//   return true
+// };
