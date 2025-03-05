907ce7f44182ebe305319991531545c578fd7ad5
Usman Khan <usman918khan@gmail.com>
2025-03-05 21:36:00 +0500

    array 8 method practiced

---
+++ Array/array1.js
@@ -0,0 +1,33 @@
+// JavaScript Array Methods
+const str1 = 'abcdefgh'
+const unsortArray = [55, 66, 9, 4, 245, 89, 85, 34, 0, 65, 2, 0]
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr2 = ['a, b, c, d, e, f, g, h, i, j'];
+const arr3 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+const arrLength = (arr) => {
+    return arr.length
+}
+console.log("array.length", arrLength(arr1));
+console.log('====================================');
+const arrConcat = (arr1, arr2) => {
+    const conCatArray = arr1.concat(arr2)
+    return conCatArray
+}
+console.log("Concated Array Method", arrConcat(arr1, arr2));
+console.log('====================================');
+
+const arrJoin = (arr1, arr2) => {
+    const res = arr1.join(' ') + arr2.join(' ')
+    return res
+
+}
+// console.log("Array join Method", arrJoin(arr3, arr1));
+// console.log('====================================');
+// console.log("Sorted Array", unsortArray.sort());
+// console.log("reverseSorted", unsortArray.sort((a, b) => b - a));
+// console.log('reverse', arr1.reverse());
+console.log('====================================');
+console.log('array shift', arr1.shift());
+console.log('array shift', arr1.unshift('banana'));
+console.log('====================================',arr1);
